94efd811c5ca74bb815c99a99e292e1b1c0b7cae
coldFish <williams_tom@163.com>
2024-11-22 20:51:46 +0800

    去掉了actionbar以及一些小问题

--- SmartOld/app/src/main/java/com/example/smartold/ui/HomeFragment.kt
+++ SmartOld/app/src/main/java/com/example/smartold/ui/HomeFragment.kt
@@ -6,17 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.smartold.R
 
 
-class HomeFragment:Fragment() {
+class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val textView = TextView(container?.context)
-        textView.text = "主页"
-        textView.textSize = 30f
-        return textView
+        // 使用 inflater 直接返回布局
+        return inflater.inflate(R.layout.home_fragment, container, false)
     }
 }
